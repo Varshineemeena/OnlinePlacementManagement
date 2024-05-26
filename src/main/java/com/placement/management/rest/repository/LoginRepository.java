8e31b15ca47ca2d225d492413f9d645fd95048e1
Varshinee <93853224+Varshineemeena@users.noreply.github.com>
2024-05-26 13:58:44 +0530

    Delete src directory

--- src/main/java/com/placement/management/rest/repository/LoginRepository.java
+++
@@ -1,7 +0,0 @@
-package com.placement.management.rest.repository;
-
-import com.placement.management.entity.Login;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginRepository extends JpaRepository<Login, String> {
-}
